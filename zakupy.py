aa093b29cbe3d96481c1b0c716f58063b01f460a
wojciech1 <wojciech11@users.noreply.github.com>
2019-10-27 14:11:27 +0100

    dodano

--- zakupy.py
+++ zakupy.py
@@ -3,23 +3,29 @@ import pprint
 import csv
 
 def save_koszyk(koszyk):
+    file = open("koszyk.csv", "w")
     for key in koszyk:
-        file = open("koszyk.csv", "w")
-        file.write("{0}:{1}".format(key, koszyk))
-        file.close()
-
+        file.write("{0}:{1}:{2}\n".format(key['name'], key['masa'], key['kolor']))
+    file.close()
+def read_koszyk():
+    file = open("koszyk.csv", "r")
+    return a == file.read()
+    print(a)
 def main():
     koszyk = [{"name" : "wiewiorka", "masa" : 300, "kolor" : "ruda"},
     {"name" : "jez", "masa" : 500, "kolor" : "szary"},
     {"name" : "mysz", "masa" : 100, "kolor" : "biala"}
     ]
-    pprint.pprint(koszyk)
+    #pprint.pprint(koszyk)
     save_koszyk(koszyk)
+    read_koszyk()
 
 if __name__ == "__main__":
     main()
-with open('koszyk.csv', 'w', encoding='utf-8') as csvfile:
-# inicjuje zapisywanie
-    csvwriter = csv.writer(csvfile)
+
+
+
+## inicjuje zapisywanie
+    #csvwriter = csv.writer(csvfile)
 #Wpisujemy pierwsza linie naszego csv
-    csvwriter.writerow('name','masa','kolor')
+    #csvwriter.writerow('name','masa','kolor')
